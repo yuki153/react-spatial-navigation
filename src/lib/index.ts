971ce4fe99760d71caca3f1b13b11340ab5f1a43
yuki153 <37236474+yuki153@users.noreply.github.com>
2022-10-25 02:22:50 +0900

    refactor: 💡 Add Direction, PublicComponentProps to export type

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,8 +2,10 @@
  * @license See https://github.com/ for full license
  */
 import { spatialNavigation, FOCUSED_SELECTOR_NAME, FOCUSED_CLASS_NAME } from "./spatial-navigation";
+import type { DirectionKeys } from "./spatial-navigation";
 export { withFocusable } from "./with-focusable";
-export type { FocusableProps } from "./spatial-navigation";
+export type { FocusableProps, PublicComponentProps } from "./spatial-navigation";
+export type Direction = DirectionKeys;
 
 const { init: initNavigation, setKeyMap } = spatialNavigation;
 
