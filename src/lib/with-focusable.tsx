2b99dbb361fe0d89c0bde0331db0d1787a47c803
yuki153 <37236474+yuki153@users.noreply.github.com>
2022-11-18 01:07:55 +0900

    perf: ⚡️ HOC が return するコンポーネントに渡す props 等々を memo 化する

--- src/lib/with-focusable.tsx
+++ src/lib/with-focusable.tsx
@@ -85,7 +85,7 @@ export const withFocusable = ({
          * receivedProps が 'T' の SubType でない事を明示化する。ジェネリクス(T)として渡された型（自身）へ as T を用いて変換する
          * TS Error の解消：型 'T' の制約に代入できますが、'T' は制約 'FocusableProps' の別のサブタイプでインスタンス化できることがあります
          */
-        const receivedProps = {
+        const receivedProps = useMemo(() => ({
             className: focused ? (className ? `${className} ${FOCUSED_CLASS_NAME}` : FOCUSED_CLASS_NAME) : className,
             focusKey: focusKey || null,
             realFocusKey,
@@ -100,7 +100,7 @@ export const withFocusable = ({
             pauseSpatialNavigation: spatialNavigation.pause.bind(spatialNavigation),
             updateAllSpatialLayouts: spatialNavigation.updateAllLayouts.bind(spatialNavigation),
             getCurrentFocusKey: spatialNavigation.getCurrentFocusKey.bind(spatialNavigation),
-        };
+        }), [focused, hasFocusedChild, parentFocusKey, preferredChildFocusKey]);
 
         useEffect(() => {
             spatialNavigation.addFocusable({
@@ -143,8 +143,10 @@ export const withFocusable = ({
             }
         })
 
+        const value = useMemo(() => ({parentFocusKey: realFocusKey}), []);
+
         return (
-            <Context.Provider value={{parentFocusKey: realFocusKey}}>
+            <Context.Provider value={value}>
                 <Component {...props as P} {...receivedProps} ref={ref}/>
             </Context.Provider>
         );
